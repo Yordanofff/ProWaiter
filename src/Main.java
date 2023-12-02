9aa14ef6e0b1ca4e2d65c07211f65c1a44fb3ee8
Yordanofff <ivailo.yordanov.ph@gmail.com>
2023-12-02 18:13:51 +0200

    Added the changes in the Main too - mainly for testing purposes.

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,83 @@
-import BackEnd.Users.UserManager;
-import FrontEnd.MenuBuilder;
-import FrontEnd.UserInterface;
-
-import java.util.ArrayList;
+import BackEnd.DB.DBOperations;
+import BackEnd.Restaurant.Dishes.Dessert;
+import BackEnd.Restaurant.Dishes.Dish;
+import BackEnd.Restaurant.Dishes.Drink;
+import BackEnd.Restaurant.Dishes.Food;
+import BackEnd.Restaurant.Menu.RestaurantMenu;
+import BackEnd.Users.*;
+import FrontEnd.*;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
-    // todo create abstract class for DB
     public static void main(String[] args) {
+
 //        runApp();
-        testTable();
+
+        testRunAppAsADMIN();
+//        testRunAppAsCOOK();
+//        testRunAppAsWaiter();
 
     }
 
-    public static void testTable() {
-        List<String> userDataToPrint = new ArrayList<>();
-        String description = "Col1f, Col2, Col3, Col4";
-        userDataToPrint.add("Hellotftftyftdtrdtyf, this is, row1 - title, another cola");
-        userDataToPrint.add("Hello, this is, , ");
-        userDataToPrint.add("Hello2, , row2, ");
+    public static void populateMenu() {
+        // Create a HashMap for Food
+        HashMap<String, Double> foodMenu = new HashMap<>();
+        foodMenu.put("Pizza", 12.99);
+        foodMenu.put("Burger", 8.49);
+        foodMenu.put("Pasta", 10.95);
+        foodMenu.put("Salad", 6.99);
 
-        MenuBuilder.printMenuOptionsInFrameNewTable(userDataToPrint, "Users", description);
+        // Create a HashMap for drinks
+        HashMap<String, Double> drinkMenu = new HashMap<>();
+        drinkMenu.put("Soda", 2.49);
+        drinkMenu.put("Iced Tea", 1.99);
+        drinkMenu.put("Smoothie", 4.95);
+
+        // Create a HashMap for desserts
+        HashMap<String, Double> dessertMenu = new HashMap<>();
+        dessertMenu.put("Chocolate Cake", 5.99);
+        dessertMenu.put("Ice Cream", 3.50);
+        dessertMenu.put("Cheesecake", 6.75);
+
+        for (HashMap.Entry<String, Double> entry : foodMenu.entrySet()) {
+            Dish food = new Food(entry.getKey(), entry.getValue());
+            RestaurantMenu.addDish(food);
+        }
+        for (HashMap.Entry<String, Double> entry : drinkMenu.entrySet()) {
+            Dish drink = new Drink(entry.getKey(), entry.getValue());
+            RestaurantMenu.addDish(drink);
+        }
+        for (HashMap.Entry<String, Double> entry : dessertMenu.entrySet()) {
+            Dish desert = new Dessert(entry.getKey(), entry.getValue());
+            RestaurantMenu.addDish(desert);
+        }
+
+        List<Dish> x = RestaurantMenu.getDishes();
+
+        for (Dish d : x) {
+            DBOperations.addDishToRestaurantMenuItems(d);
+        }
     }
 
     public static void runApp() {
-        System.out.println(UserManager.getActiveUsers());
-        int activeUsers = UserManager.getActiveUserCount();  // todo - move getUsers in UserManager
-//        System.out.println("Active now: " + activeUsers);
-//
         UserInterface.startApp();
-//
-//        activeUsers = UserManager.getActiveUserCount();  // todo - move getUsers in UserManager
-//        System.out.println("Active now: " + activeUsers);
-
     }
+
+    public static void testRunAppAsADMIN(){
+        User user = new Administrator("Ivo", "Yordanov", "admin", "123");
+        MenuBuilder.AdminMenu(user);
+    }
+
+    public static void testRunAppAsCOOK(){
+        User user = new Cook("Ivo", "Yordanov", "cook1", "123");
+        MenuBuilder.KitchenMenu(user);
+    }
+
+    public static void testRunAppAsWaiter(){
+        User user = new Waiter("Ivo", "Yordanov", "waiter1", "123");
+        MenuBuilder.WaiterMenu(user);
+    }
+
 }
+
