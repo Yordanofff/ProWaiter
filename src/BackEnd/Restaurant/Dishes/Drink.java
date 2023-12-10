ab0e043f85706c57d966070bf61927becac317c2
Yordanofff <ivailo.yordanov.ph@gmail.com>
2023-12-10 14:50:25 +0200

    need to push

--- src/BackEnd/Restaurant/Dishes/Drink.java
+++ src/BackEnd/Restaurant/Dishes/Drink.java
@@ -3,10 +3,6 @@ package BackEnd.Restaurant.Dishes;
 public class Drink extends Dish {
     public static final DishType dishType = DishType.DRINK;
 
-    public Drink() {
-        super(dishType);
-    }
-
     public Drink(String name, double price) {
         super(name, price, dishType);
     }
